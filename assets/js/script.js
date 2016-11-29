6e9d327218ff70fd9e82a7fdbb65cd9264879990
pingbo97 <pingbozhou@berkeley.edu>
2016-11-29 04:43:09 -0800

    ert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
 	
 	
-	$("#link").click(function(){
+	$("#action").click(function(){
 		$("#modal-container").show();
 	});
+
+	$("#otheraction").click(function(){
+		$("#modal-container").show();
+	});
+	
 	
 	$(".submit").click(function(){
 		$("#modal-container").hide();
