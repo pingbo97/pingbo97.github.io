83077ca2c156106ea18d0b8fa65455f4f527bebb
pingbo97 <pingbozhou@berkeley.edu>
2016-11-29 18:54:44 -0800

    est

---
+++ loginassets/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	
+	
+	$("#signup").click(function(){
+		$("#modal-box").show();
+	});
+	
+
+
+	$("#modal-overlay").click(function(){
+		$("#modal-container").hide();
+	});
+});
